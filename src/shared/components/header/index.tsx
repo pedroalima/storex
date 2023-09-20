e3471044d86a99f6fbdef1b78a1dc7c5c8aac367
Pedro Lima <tsc.pedrolima@gmail.com>
2023-09-20 17:32:24 -0300

    style: add design header

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,8 +1,10 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { Badge, BadgeProps, Box, Button, IconButton, styled } from "@mui/material";
+import * as M from "@mui/material";
 
 export const Header = () => {
-	const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+	const theme = M.useTheme();
+
+	const StyledBadge = M.styled(M.Badge)<M.BadgeProps>(({ theme }) => ({
 		"& .MuiBadge-badge": {
 			right: -3,
 			top: 13,
@@ -12,16 +14,28 @@ export const Header = () => {
 	}));
 
 	return (
-		<Box display="flex" justifyContent="space-between" alignItems="center">
-			<h1>Store X</h1>
-			<Box>
-				<Button variant="contained">Sign in</Button>
-				<IconButton aria-label="cart">
-					<StyledBadge badgeContent={4} color="secondary">
-						<ShoppingCart />
-					</StyledBadge>
-				</IconButton>
-			</Box>
-		</Box>
+		<M.Box sx={{ flexGrow: 1 }}>
+			<M.AppBar position="static" sx={{
+				paddingX: theme.spacing(2), 
+				background: theme.palette.primary.dark
+			}} >
+				<M.Toolbar>
+					<M.Typography variant="h6" component="div" sx={{ 
+						flexGrow: 1, 
+						fontWeight:700 
+					}}>Store X</M.Typography>
+					<M.Box display="flex" justifyContent="space-between" sx={{
+						width: "40%",
+					}}>
+						<M.Button variant="contained" color="secondary" size="small" sx={{ fontWeight:800 }}>Sign in</M.Button>
+						<M.IconButton aria-label="cart" color="secondary">
+							<StyledBadge badgeContent={4} color="secondary">
+								<ShoppingCart />
+							</StyledBadge>
+						</M.IconButton>
+					</M.Box>
+				</M.Toolbar>
+			</M.AppBar>
+		</M.Box>
 	);
 };
--- src/shared/theme/Theme.ts
+++ src/shared/theme/Theme.ts
@@ -1,23 +1,25 @@
 import { createTheme } from "@mui/material";
-import { cyan, blue } from "@mui/material/colors";
 
 export const Theme = createTheme({
 	palette: {
 		primary: {
-			main: cyan[800],
-			light: cyan[700],
-			dark: cyan[900],
+			main: "#333942",
+			light: "#515761",
+			dark: "#131921",
 			contrastText: "#FFFFFF",
 		},
 		secondary: {
-			main: blue[800],
-			light: blue[700],
-			dark: blue[900],
-			contrastText: "#FFFFFF",
+			main: "#f1f0ed",
+			light: "#f7f6f3",
+			dark: "#e3e2df",
+			contrastText: "#242321",
 		},
 		background: {
 			default: "#F7F6F3",
 			paper: "#FFFFFF",
 		}
+	},
+	typography: {
+		fontFamily: "Oxygen",
 	}
 });
