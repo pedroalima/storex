676f551af247af2f825b96aa062215996e49a3ab
Pedro Lima <85854788+pedroalima@users.noreply.github.com>
2023-09-29 22:01:33 -0300

    feat: add sign in page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 // Pages
 import { Home } from "../pages/home";
+import { SignIn } from "../pages/sign-in";
+import { Inventory } from "../shared/components/inventory";
 
 export const AppRoutes = () => {
 	return (
 		<Routes>
-			<Route path="/" element={ <Home /> } />
-
+			<Route path="/" element={ <Home /> }>
+				<Route path="/" element={ <Inventory /> } />
+				<Route path="/sign-in" element={ <SignIn /> } />
+			</Route>
+		
 			<Route path="*" element={ <Navigate to="/" /> } />
 		</Routes>
 	);
