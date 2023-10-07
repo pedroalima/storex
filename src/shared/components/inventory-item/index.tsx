62b4815299ae79ce3e120c3e839d9de0677ae56b
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-06 21:46:54 -0300

    feat: add responsive cards

--- src/shared/components/inventory-item/index.tsx
+++ src/shared/components/inventory-item/index.tsx
@@ -13,6 +13,9 @@ export type ProductType = {
 export const InventoryItem = ({ product } : {product: ProductType }) => {
 	const { name, price, img } = product;
 	const theme = M.useTheme();
+	const smDown = M.useMediaQuery(theme.breakpoints.down("sm"));
+	const mdDown = M.useMediaQuery(theme.breakpoints.down("md"));
+	const lgDown = M.useMediaQuery(theme.breakpoints.down("lg"));
 
 	const dispatch = useDispatch();
 
@@ -21,12 +24,18 @@ export const InventoryItem = ({ product } : {product: ProductType }) => {
 	};
 
 	return (
-		<M.Card>
-			<M.Box paddingY={theme.spacing(2)} display="flex" justifyContent="center" >
-				<M.CardMedia sx={{ width:"60%", height: 250 }} image={img} title={name} />
-			</M.Box>
+		<M.Card sx={ 
+			smDown ? 
+				{ width: "100%", padding: theme.spacing(3), display: "flex", flexDirection: "column" } :
+				mdDown ?
+					{ width: "240px", padding: theme.spacing(3), display: "flex", flexDirection: "column" } :
+					lgDown ?
+						{ width: "250px", padding: theme.spacing(3), display: "flex", flexDirection: "column" } :
+						{ width: "18%", padding: theme.spacing(3), display: "flex", flexDirection: "column" }
+		}>
+			<M.CardMedia sx={{ width:"200px", height: "200px", alignSelf: "center" }} image={img} title={name} />
 			
-			<M.CardContent>
+			<M.CardContent sx={{paddingX: "0", paddingY: theme.spacing(2)}}>
 				<M.Typography 
 					gutterBottom 
 					variant="h5" 
@@ -36,7 +45,7 @@ export const InventoryItem = ({ product } : {product: ProductType }) => {
 					component="span">{currency(price)}</M.Typography>
 			</M.CardContent>
 
-			<M.CardActions>
+			<M.CardActions sx={{padding: "0"}}>
 				<M.Button 
 					size="small" 
 					variant="contained" 
